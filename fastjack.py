d265eda2ad568cb57e269da60f11291d782a27ed
neo-jack-official <59382524+neo-jack-official@users.noreply.github.com>
2020-06-04 07:00:27 +0000

    Actualizacion de Servidor verificador de ip
    se cambia servidor de verificacion de Ip

--- fastjack.py
+++ fastjack.py
@@ -129,7 +129,7 @@ def usage():
 
 tor = True
 if tor is True:
-    ipcheck_url = 'http://canihazip.com/s'
+    ipcheck_url = 'http://icanhazptr.com''
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050) 
     socket.socket = socks.socksocket
     try:       
